a7589c2c84a8a731fbb7259196d8e856465265e5
Ralph Tang <r33tang@uwaterloo.ca>
2018-04-06 02:16:18 -0400

    Add ternary weight scaling

--- candle/quantize/dorefa.py
+++ candle/quantize/dorefa.py
@@ -17,11 +17,19 @@ class TernaryClampFunction(ag.Function):
     @staticmethod
     def forward(ctx, x):
         delta = 0.7 * x.abs().mean()
-        x = x / delta
-        return x.clamp(-1, 1).int().float()
+        new_x = x / delta
+        new_x = new_x.clamp(-1, 1).int().float()
+        factor = x[new_x.abs().byte()]
+        ctx.alpha = None
+        if factor.numel() > 0:
+            ctx.alpha = alpha = factor.abs().mean()
+            return alpha * new_x
+        return new_x
 
     @staticmethod
     def backward(ctx, grad_output):
+        if ctx.alpha is not None:
+            return grad_output * ctx.alpha
         return grad_output
 
 class QuantizeKFunction(ag.Function):
@@ -41,15 +49,23 @@ ternary_clamp = TernaryClampFunction.apply
 quantize_k = QuantizeKFunction.apply
 
 class TernaryWeightHook(ProxyDecorator):
-    def __init__(self, layer, child):
+    def __init__(self, layer, child, chunk=1, chunk_dim=0):
         super().__init__(layer, child)
+        self.chunk = chunk
+        self.chunk_dim = chunk_dim
 
     @property
     def sizes(self):
         return self.child.sizes
 
+    def _chunk_apply(self, x):
+        chunks = [ternary_clamp(x) for x in x.chunk(self.chunk, dim=self.chunk_dim)]
+        return torch.cat(chunks, self.chunk_dim)
+
     def call(self, input):
-        return input.apply_fn(ternary_clamp)
+        if self.chunk == 1:
+            return input.apply_fn(ternary_clamp)
+        return input.apply_fn(self._chunk_apply)
 
 class DoReFaActivation(nn.Module):
     def __init__(self, k):
@@ -65,10 +81,10 @@ class TernaryQuantizeContext(Context):
         self.soft = soft
         self.scale_alpha = scale_alpha
 
-    def activation(self, k=2):
+    def activation(self, k=4):
         return self.bypass(DoReFaActivation(k))
 
-    def compose(self, layer, **kwargs):
+    def compose(self, layer, chunk=1, chunk_dim=0, **kwargs):
         layer = super().compose(layer, **kwargs)
-        hook = layer.hook_weight(TernaryWeightHook)
+        hook = layer.hook_weight(TernaryWeightHook, chunk=chunk, chunk_dim=chunk_dim)
         return layer
